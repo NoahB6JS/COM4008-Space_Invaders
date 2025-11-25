28e7faae09d87f23cca4aed9dd7022b9780785d3
NoahB6JS <noah.js.b6@gmail.com>
2025-11-25 21:54:44 +0000

    Bliting invaders
    - Creating the invaders variables for spacing and positioning.
    - Draw invaders function positions invaders and can be reused for restarting game.
    - Had to Indent the run screen code or else the screen wouldn't update.
    
    Invaders now spaced out on screen when ran.

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ squid_img = pygame.image.load("Media/squid.png")
 ufo_img = pygame.image.load("Media/ufo.png")
 bg_img = pygame.image.load("Media/bg.jpg")
 
-#Classes
 
+#----Classes
 class Player:
     def __init__(self, x, y, img, l, h, score, lives):
         self.x = x
@@ -36,22 +36,42 @@ class Invader:
         self.x = x
         self.y = y
         self.img = pygame.transform.scale(img, (l, h))
-        self.rect = pygame.Rect(self.x, self.y, self.l, self.h)
         self.l = l
         self.h = h
-        self.direction = -1  
         self.health = health
+        self.rect = pygame.Rect(self.x, self.y, self.l, self.h)
+        self.speed = 0.75
+        self.direction = -1  
         self.bullet_type = bullet_type
         
     def update(self):
         self.rect = pygame.Rect(self.x, self.y, self.l, self.h)
         
 class Bullet:
-    def __init__(self):
+    def __init__(self, x, y, w, h, s):
+        self.x = x
+        self.y = y
+        self.speed = s
+        self.width = w
+        self.height = h
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         
+    def update(self):
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         
-        
-        
+class EnemyBullet:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.w = 4
+        self.h = 10
+        self.speed = 5
+        self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+
+    def update(self):
+        self.y += self.speed
+        self.rect.topleft = (self.x, self.y)
+    
 
 #--Main Game variables
 
@@ -60,10 +80,41 @@ clock = pygame.time.Clock()
 
 SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 500
-screen = pygame.display.set_mode([SCREEN_HEIGHT, SCREEN_WIDTH])
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 score = 0
 level = 0
+
+#Invader starting positions in matrix
+
+invaders = []
+invader_startrow = 10
+invader_endrow = 300
+invader_startcol = 100
+invader_endcol = 400 
+
+#Draw invaders function
+
+def draw_invaders():
+    START_X = 100
+    SPACING_X = 20
+    START_Y = 30
+    SPACING_Y = 25
+    ROWS = 5
+    COLS = 8
+    
+    for r in range(ROWS):
+        for c in range(COLS):
+            x = START_X + c * SPACING_X
+            y = START_Y + r * SPACING_Y
+            
+            health = 1
+            bullet_type = "easy"
+            
+            invaders.append(Invader(x,y,defender_img,30,30,health,bullet_type))
+            
+draw_invaders()          
+            
 
 #--Contains the main game loop
 
@@ -76,8 +127,14 @@ while running:
             pygame.quit()
             sys.exit()
             
-   
-#Runs screen           
-pygame.display.flip()
-clock.tick(FPS)
+            
+    screen.blit(bg_img, (0,0))
+            
+    for inv in invaders:
+        screen.blit(inv.img, (inv.x, inv.y))
+        inv.update()
+        
+    #Runs screen           
+    pygame.display.flip()
+    clock.tick(FPS)
 
