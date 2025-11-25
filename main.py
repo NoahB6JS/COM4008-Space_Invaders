82b55c973d7066e4c33cb565da6c9fcd2f317f35
NoahB6JS <noah.js.b6@gmail.com>
2025-11-25 15:46:27 +0000

    update to main.py
    - Constants and variables added
    - Main game loop
    - Screen now generated when code is ran

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+#Imported modules
+
 import sys
 import pygame
 import random
+
+pygame.init()
+pygame.mixer.init()
 
 #--Image Files loaded in and set to variables
 
@@ -11,4 +16,31 @@ squid_img = pygame.image.load("Media/squid.png")
 ufo_img = pygame.image.load("Media/ufo.png")
 bg_img = pygame.image.load("Media/bg.jpg")
 
+#--Main Game variables
+
+FPS = 60
+clock = pygame.time.Clock()
+
+SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 500
+screen = pygame.display.set_mode([SCREEN_HEIGHT, SCREEN_WIDTH])
+
+score = 0
+level = 0
+
+#--Contains the main game loop
+
+running = True
+while running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            sys.exit()
+            
+   
+#Runs screen           
+pygame.display.flip()
+clock.tick(FPS)
 
