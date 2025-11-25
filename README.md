d55ffa7a8be37405445b4aceb800aaaae04b61f6
NoahB6JS <noah.js.b6@gmail.com>
2025-11-25 15:35:40 +0000

    Image variables created

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # COM4008-Space_Invaders
 Space Invaders written in Python. Group project coded by 3 members. Documented development and testing for the solution included in Repository.
+
+
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import sys
+import pygame
+import random
+
+#--Image Files loaded in and set to variables
+
+player_img = pygame.image.load("Media/player.png")
+defender_img = pygame.image.load("Media/defender.png")
+alien_img = pygame.image.load("Media/alien.png")
+squid_img = pygame.image.load("Media/squid.png")
+ufo_img = pygame.image.load("Media/ufo.png")
+bg_img = pygame.image.load("Media/bg.jpg")
+
 
