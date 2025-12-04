ae92cffd84f2be4b77820e6104d14e96638d5c9a
mohabxz493-art <mohab.xz493@gmail.com>
2025-12-04 13:53:49 +0000

    Update main.py
    Bullet movement for the player:

--- main.py
+++ main.py
@@ -163,3 +163,24 @@ while running:
 if keys[pygame.K_SPACE] and player.cooldown_counter == 0:
     game.player_bullets.append(Bullet(player.x + 18, player.y, 4, 10, -7, "player"))
     player.cooldown_counter = player.cooldown
+     # Player bullet movement
+for bullet in game.player_bullets[:]:
+    bullet.update()
+    pygame.draw.rect(screen, (255,255,0), bullet.rect)
+    if bullet.y < 0:
+        game.player_bullets.remove(bullet)
+
+# Hit invaders
+for inv in game.invaders[:]:
+    if bullet.rect.colliderect(inv.rect):
+        inv.health -= 1
+        game.player_bullets.remove(bullet)
+        if inv.health <= 0:
+            game.score += inv.point_value
+game.invaders.remove(inv)
+break
+if player.cooldown_counter > 0:
+     player.cooldown_counter -= 1
+
+
+
