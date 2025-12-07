32896c4177dd597bf82c723cbd9e6e2c70949d39
Noah John Samuel Blanchard <noah.js.b6@gmail.com>
2025-12-07 18:27:26 +0000

    second pytest method
    check if user health works

--- test_game.py
+++ test_game.py
@@ -1,12 +1,12 @@
-from game_assets import Invader
+from game_assets import Invader, Defender
 import pygame
 import os
 
 image_path = os.path.join("Media", "invader.png") #load image path
 invader_img = pygame.image.load(image_path)
 
-class TestInvader:
-    def test_take_damage(self): #test function
+class TestGame:
+    def test_invader_take_damage(self): #test function
 # tests if invader will die after loosing 3 lives
         invader = Invader(0, 0, invader_img, 50, 50, health=3, bullet_speed=5, point_value=100, fire_rate=0.1)
         assert invader.take_damage() == False  
@@ -15,3 +15,13 @@ class TestInvader:
         assert invader.health == 1
         assert invader.take_damage() == True   
         assert invader.health == 0
+
+    def test_player_takes_damage(self):
+        player = Defender(0, 0, invader_img, 40, 40, cooldown=20)
+        player.lives = 3  
+        player.lives -= 1  
+        assert player.lives == 2
+        player.lives -= 1  
+        assert player.lives == 1
+        player.lives -= 1  
+        assert player.lives == 0
